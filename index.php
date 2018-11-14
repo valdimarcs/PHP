ba1ea627870eff2433e0e38390a6ae72f1b2cd8c
Aluno <aluno@edu.senac.br>
2018-11-14 21:45:07 -0200

    começou o projeto

---
+++ index.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+<h1>
+	<?php
+	$nome = "Geovane";
+	echo  $nome; // esse é um comentário
+	/* esse comentario é de 
+	varias linhas de texto e pode aumentar
+	*/
+	#outro comentário
+
+?>
+</h1>
+</body>
+</html>
+
